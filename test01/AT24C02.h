361ed209f92622fc152ac782ba585f6dc5235adf
ariyflow <ariyflow4342@gmail.com>
2026-03-23 22:00:48 +0800

    更新感知层功能设计文档

--- test01/AT24C02.h
+++ test01/AT24C02.h
@@ -5,8 +5,8 @@
 0,1 - 保存点赞数 0保存高8bit 1保存低8bit
 */
 
-extern void ATInit();
-extern unsigned char rAT(unsigned char addr);
-extern void wAT(unsigned char addr, unsigned char val);
+extern void ATInit(); // 初始化
+extern unsigned char rAT(unsigned char addr); // 读取addr的数据
+extern void wAT(unsigned char addr, unsigned char val); // 写入数据
 
 #endif
