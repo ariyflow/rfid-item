2234bec4d4576a6fd87377203aa9edcf3ee99d73
ariyflow <ariyflow4342@gmail.com>
2026-03-23 22:45:02 +0800

    update structure of sensor-layer, it will extend in the next update.

--- test01/main.c
+++ test01/main.c
@@ -15,6 +15,7 @@ xdata unsigned char receive_buf[24]; // 接收缓冲区
 xdata unsigned char status = 0x0; // 临时保存状态
 xdata unsigned char led_vector = 0x01; // led状态
 xdata ADC adc_data; // 保存获取的adc数据
+xdata unsigned char mode = 0x00; // 当前的模式
 
 
 // 计算buf数组的偶校验结果值
@@ -116,85 +117,133 @@ unsigned char writeRFID(unsigned char _addr, unsigned char* _data){
 	return tmp;
 }
 
+void commit_sensor_data(){
+	adc_data = getAdc();
+	
+	send_buf[2]=0x08;
+	send_buf[3]=0x03;
+	send_buf[4]=(adc_data.adcTem>>8); // 温度
+	send_buf[5]=(adc_data.adcTem&0xff);
+	send_buf[6]=(adc_data.adcLum>>8); // 光照
+	send_buf[7]=(adc_data.adcLum&0xff);
+	send_buf[8]=(adc_data.adcHall); // 霍尔
+	send_buf[9]=0; // 振动
+	send_buf[10]=set_checksum(send_buf, 10);
+	uart1Send(send_buf, 11);
+
+}
+
 void my100msCallback(){
 
 }
 
 void myKeyCallback(){
 	xdata unsigned char key;
-	key = getKeyAct(enumKey1);
-	if(key == enumKeyPress){
-		// WriteRawRC(CommandReg,PCD_RESETPHASE);
 
-//		send_buf[0] = ReadRawRC(CommandReg);
-//		send_buf[1] = ReadRawRC(ModeReg);
-//		send_buf[2] = ReadRawRC(TReloadRegL);
-//		send_buf[3] = ReadRawRC(TReloadRegH);
-//		send_buf[4] = ReadRawRC(TModeReg);
-//		send_buf[5] = ReadRawRC(TPrescalerReg);
-//		send_buf[6] = ReadRawRC(TxAskReg);
-//		send_buf[7] = 0xAA;
-//		uart1Send(send_buf, 8);
-//		setLed(0xa3);
-
-		// 获取uid
-		if(read_uid(uid)!=0x07)return;
-		uart1Send(uid, 4);
-	}
+	// key2是模式切换键，与其余逻辑分离
 	key = getKeyAct(enumKey2);
-	if(key == enumKeyPress){
+	if(key == enumKeyPress){ // key2按下，切换模式
+		// 切换前的操作
 
-		// 返回序列号和地址20的数据
-		status = readRFID(20, uid, card_data);
-		if(status != 0x2F){ // 失败返回状态码
-			send_buf[2] = status;
-			uart1Send(send_buf, 3);
+		mode = (mode+1)%3;
+
+		// 切换后的操作
+		if(mode == 1){
+			setLed(0x00);
+			setNum(0);
 		}
-		else{
-			uart1Send(uid, 4);
-			uart1Send(card_data, 16);
-		}
-		// do {
-		// 	status = readRFID(20, uid, card_data);
-		// }
-		// while(status != 0x2f);
-		// uart1Send(uid, 4);
-		// uart1Send(card_data, 16);
 	}
-	// ReadCardData(uid, card_buf);
+
+	if(mode == 0){ // MODE0 待机模式
+		key = getKeyAct(enumKey1);
+		if(key == enumKeyPress){ // key1 按下，获取RFID序列号
+			// WriteRawRC(CommandReg,PCD_RESETPHASE);
+
+	//		send_buf[0] = ReadRawRC(CommandReg);
+	//		send_buf[1] = ReadRawRC(ModeReg);
+	//		send_buf[2] = ReadRawRC(TReloadRegL);
+	//		send_buf[3] = ReadRawRC(TReloadRegH);
+	//		send_buf[4] = ReadRawRC(TModeReg);
+	//		send_buf[5] = ReadRawRC(TPrescalerReg);
+	//		send_buf[6] = ReadRawRC(TxAskReg);
+	//		send_buf[7] = 0xAA;
+	//		uart1Send(send_buf, 8);
+	//		setLed(0xa3);
+
+			// 获取uid
+			if(read_uid(uid)!=0x07)return;
+			uart1Send(uid, 4);
+		}
+		// ReadCardData(uid, card_buf);
+
+	}
+
 }
 
 void myADCKeyCallback(){
 	xdata unsigned char key;
-	key = getADCKeyAct(enumADCKey3);
-	if(key == enumKeyPress){
 
-		// 向地址20写入数据
-		send_buf[2] = writeRFID(20, test_data);
-		uart1Send(send_buf, 3);
-	}
-	key = getADCKeyAct(enumADCKeyUp);
-	if(key == enumKeyPress){
+	if(mode == 0){ // MODE0 待机模式
+		
+		key = getADCKeyAct(enumADCKey3);
+		// setLed(key);
+		if(key == enumKeyPress){ // key3按下，返回RFID序列号和地址20的数据
+			
+			// 返回序列号和地址20的数据
+			status = readRFID(20, uid, card_data);
+			if(status != 0x2F){ // 失败返回状态码
+				send_buf[2] = status;
+				uart1Send(send_buf, 3);
+			}
+			else{
+				uart1Send(uid, 4);
+				uart1Send(card_data, 16);
+			}
+			// do {
+			// 	status = readRFID(20, uid, card_data);
+			// }
+			// while(status != 0x2f);
+			// uart1Send(uid, 4);
+			// uart1Send(card_data, 16);
+		}
 
-	}
-	key = getADCKeyAct(enumADCKeyDown);
-	if(key == enumKeyPress){
+		key = getADCKeyAct(enumADCKeyCenter);
+		if(key == enumKeyPress){
 
-	}
-	key = getADCKeyAct(enumADCKeyLeft);
-	if(key == enumKeyPress){
-		// 写入失败，返回失败的数据帧
-		// 包头2+数据字节1+指令字节1+返回状态1+偶校验1=6
-		send_buf[2]=0x03;
-		send_buf[3]=0x02;
-		send_buf[4]=0x00;
-		send_buf[5]=set_checksum(send_buf, 5);
-		uart1Send(send_buf, 6);
-	}
-	key = getADCKeyAct(enumADCKeyRight);
-	if(key == enumKeyPress){
+			// 向地址20写入数据
+			send_buf[2] = writeRFID(20, test_data);
+			uart1Send(send_buf, 3);
+		}
 
+		key = getADCKeyAct(enumADCKeyUp);
+		if(key == enumKeyPress){
+			commit_sensor_data();
+
+		}
 	}
+
+	// key = getADCKeyAct(enumADCKeyUp);
+	// if(key == enumKeyPress){
+
+	// }
+	// key = getADCKeyAct(enumADCKeyDown);
+	// if(key == enumKeyPress){
+
+	// }
+	// key = getADCKeyAct(enumADCKeyLeft);
+	// if(key == enumKeyPress){
+	// 	// 写入失败，返回失败的数据帧
+	// 	// 包头2+数据字节1+指令字节1+返回状态1+偶校验1=6
+	// 	send_buf[2]=0x03;
+	// 	send_buf[3]=0x02;
+	// 	send_buf[4]=0x00;
+	// 	send_buf[5]=set_checksum(send_buf, 5);
+	// 	uart1Send(send_buf, 6);
+	// }
+	// key = getADCKeyAct(enumADCKeyRight);
+	// if(key == enumKeyPress){
+
+	// }
 	
 }
 
@@ -206,8 +255,6 @@ void myUartCallback(){
 	// 回显
 	// uart1Send(receive_buf, 24);
 	// return;
-
-
 
 	// 收到数据后，首先进行偶校验判断
 	if(receive_buf[23] != set_checksum(receive_buf, 23)) return;
@@ -315,18 +362,7 @@ void myUartCallback(){
 		// 03 - 获取传感器数据，此时传输传感器数据即可
 		// 包头2+数据字节1+指令字节1+温度2+光照2+霍尔1+振动1+偶校验1=11
 		
-		adc_data = getAdc();
-		
-		send_buf[2]=0x08;
-		send_buf[3]=0x03;
-		send_buf[4]=(adc_data.adcTem>>8); // 温度
-		send_buf[5]=(adc_data.adcTem&0xff);
-		send_buf[6]=(adc_data.adcLum>>8); // 光照
-		send_buf[7]=(adc_data.adcLum&0xff);
-		send_buf[8]=(adc_data.adcHall); // 霍尔
-		send_buf[9]=0; // 振动
-		send_buf[10]=set_checksum(send_buf, 10);
-		uart1Send(send_buf, 11);
+		commit_sensor_data();
 	}
 	else if(command == 0x04){
 		/* 04 - 设置从机序列号，此时需要设置从机序号为receive_buf[3:9]
@@ -428,7 +464,7 @@ void main() {
 	setSeg(0,1,2,3,4,5,6,7);
 
 	rc522Init();
-	delay_ms(1000);
+	// delay_ms(1000);
     
 	while(1) {
 			// ver = ReadRawRC(VersionReg);
