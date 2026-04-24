36d203cd43071ea0f398fcf841bd6878a6cc2fb0
ariyflow <ariyflow4342@gmail.com>
2026-04-24 17:54:35 +0800

    update: 增加配置文件加载，通过配置加载config.py来加载ROOT_USERNAME

--- server-layer/utils/config.py
+++ server-layer/utils/config.py
@@ -1,2 +1,25 @@
 import yaml
+import os
+from model.logger import log
 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CONFIG_FILE_NAME = "config.yaml"
+ENCODING = "UTF-8"
+
+_config = None
+
+def get_config() -> dict | None:
+    global _config
+    if _config is not None:
+        return _config
+    
+    path = os.path.join(ROOT_DIR, CONFIG_FILE_NAME)
+    try:
+        with open(path, "r", encoding=ENCODING) as file:
+            _config = yaml.safe_load(file)
+
+        return _config
+    except Exception as e:
+        log.error(f"运行函数[get_config]时出错： {e}")
+    
+    return None
--- server-layer/utils/root_manage.py
+++ server-layer/utils/root_manage.py
@@ -1,26 +1,31 @@
 import secrets
 from model.dbObject import db
 from model.logger import log
-
-ROOT_USERNAME = "root"
+from utils.config import get_config
 
 def ensure_root_user():
     """确保 root 用户存在，每次启动时更新其密码为新随机密码"""
+    root_username = get_config().get("ROOT_USERNAME", None)
+
+    if not root_username:
+        log.critical("运行未找到ROOT_USERNAME! 请配置该项！")
+        root_username = "root"
+
     password = secrets.token_hex(16) # 16字节
-    result = db.get_auth_pwd(ROOT_USERNAME)
+    result = db.get_auth_pwd(root_username)
     if result["status"] == "not_found":
-        db.add_auth(ROOT_USERNAME, password)
-        log.info(f"Root 用户已创建，用户名: {ROOT_USERNAME}, 密码: {password}")
+        db.add_auth(root_username, password)
+        log.info(f"Root 用户已创建，用户名: {root_username}, 密码: {password}")
         print(f"\n{'='*60}")
         print(f"Root 用户已创建！")
-        print(f"用户名: {ROOT_USERNAME}")
+        print(f"用户名: {root_username}")
         print(f"密码: {password}")
         print(f"{'='*60}\n")
     else:
-        db.update_auth(ROOT_USERNAME, password)
-        log.info(f"Root 密码已更新，用户名: {ROOT_USERNAME}, 密码: {password}")
+        db.update_auth(root_username, password)
+        log.info(f"Root 密码已更新，用户名: {root_username}, 密码: {password}")
         print(f"\n{'='*60}")
         print(f"Root 密码已更新！")
-        print(f"用户名: {ROOT_USERNAME}")
+        print(f"用户名: {root_username}")
         print(f"密码: {password}")
         print(f"{'='*60}\n")
